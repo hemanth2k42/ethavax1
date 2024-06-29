78fcc744e0bd82fa9c61c08c23b58e9d65f2c8b5
hemanth2k42 <161370698+hemanth2k42@users.noreply.github.com>
2024-06-29 16:58:10 +0530

    Update Hemanth.sol

--- Hemanth.sol
+++ Hemanth.sol
@@ -9,28 +9,28 @@ Upload your solution to GitHub and share the link with us along with a quick cod
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract SimpleEventRegistration {
-    uint public participantAge;
+contract HorrorMovie{
+    uint public viewerage;
 
     function register(uint _age) public {
-        participantAge = _age;
+        viewerage = _age;
     }
 
     function checkAssert() public view {
-        // Assert: Participant age must be between 21 and 65
-        assert(participantAge >= 21 && participantAge <= 65);
+   
+        assert(viewerage >= 18 && viewerage <= 60);
     }
 
     function checkRequire() public view {
-        // Require: Participant must be at least 21 years old and under 66 years old
-        require(participantAge >= 21, "You must be at least 21 years old to register for the event");
-        require(participantAge <= 65, "You must be under 66 years old to register for the event");
+     
+        require(viewerage >= 18, "You must be at least 18 years old to watch this movie");
+        require(viewerage <= 60, "You must be under 60 years old to watch this movie");
     }
 
     function checkRevert() public view {
-        // Revert: If age is not between 21 and 65
-        if (participantAge < 21 || participantAge > 65) {
-            revert("You must be between 21 and 65 years old to register for the event");
+       
+        if (viewerage < 18 || viewerage > 60) {
+            revert("You must be between 21 and 60 years old to atch this movie");
         }
     }
 }
